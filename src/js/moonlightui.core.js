d33ba0aef8949ab6bbafa830ef6a52307a16ceaf
Gerhard Richard Edens <richard.edens@annalect.com>
2016-10-19 12:47:53 +0200

    Trying "me".

--- src/js/moonlightui.core.js
+++ src/js/moonlightui.core.js
@@ -15,16 +15,17 @@
         tempModule,
         routerInit = false;
 
+    var me = this;
+    
     $.fn.extend({
         /* MOONLIGHTUI - System */
         checkRoute: function() {
-            var self = this;
             if (window.location.hash !== '') {
                 var url = window.location.hash.replace('#!','').replace('#','');
                 console.log(labelLib + 'Found url: ' + url);
                 for (var i = 0; i < routes.length; i++) {
                     if (routes[i].url === url) {
-                        var view = self.getView(routes[i].module,routes[i].name);
+                        var view = me.getView(routes[i].module,routes[i].name);
                         if (typeof view.__cachedOptions !== 'undefined' && view.__cachedOptions !== false) {
                             view.render(function () {}, view.__cachedOptions);
                         } else {
--- src/js/moonlightui.js
+++ src/js/moonlightui.js
@@ -14,17 +14,17 @@
         labelLib = 'MOONLIGHTUI - ',
         tempModule,
         routerInit = false;
+    var me = this;
 
     $.fn.extend({
         /* MOONLIGHTUI - System */
         checkRoute: function() {
-            var self = this;
             if (window.location.hash !== '') {
                 var url = window.location.hash.replace('#!','').replace('#','');
                 console.log(labelLib + 'Found url: ' + url);
                 for (var i = 0; i < routes.length; i++) {
                     if (routes[i].url === url) {
-                        var view = self.getView(routes[i].module,routes[i].name);
+                        var view = me.getView(routes[i].module,routes[i].name);
                         if (typeof view.__cachedOptions !== 'undefined' && view.__cachedOptions !== false) {
                             view.render(function () {}, view.__cachedOptions);
                         } else {
